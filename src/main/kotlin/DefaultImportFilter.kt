4ef46ecf24033583228986fbcc5d1bbb6f592735
Tamás Szincsák <tamas@appcorner.eu>
2022-12-20 17:08:09 +0100

    Implemented importing CSV files

---
+++ src/main/kotlin/DefaultImportFilter.kt
@@ -0,0 +1,61 @@
+import com.healthmarketscience.jackcess.Column
+import com.healthmarketscience.jackcess.ColumnBuilder
+import com.healthmarketscience.jackcess.DataType
+import com.healthmarketscience.jackcess.util.ImportFilter
+import java.io.IOException
+import java.sql.ResultSetMetaData
+import java.time.Duration
+import java.time.LocalDateTime
+
+class DefaultImportFilter(val columns: List<Column>) : ImportFilter {
+    private val columnTypes: Array<DataType> = columns.map { it.type }.toTypedArray()
+
+    override fun filterColumns(destColumns: MutableList<ColumnBuilder>, srcColumns: ResultSetMetaData?): MutableList<ColumnBuilder> {
+        return destColumns
+    }
+
+    override fun filterRow(row: Array<Any?>): Array<Any?> {
+        for (index in row.indices) {
+            // convert empty strings to null
+            if (row[index] == "") {
+                row[index] = null
+                continue
+            }
+
+            // handle special column types
+            when (columnTypes[index]) {
+                DataType.SHORT_DATE_TIME, DataType.EXT_DATE_TIME -> {
+                    if (row[index] != null)
+                        row[index] = parseDateTimeColumn(row[index] as String)
+                }
+                else -> {
+                    // nothing to do
+                }
+            }
+        }
+
+        return row
+    }
+
+    private fun parseDateTimeColumn(value: String): Any? {
+        when {
+            TIME_REGEX.matches(value) -> {
+                // HH:MM:SS duration
+                val iso = value.replaceFirst(Regex("^(\\d{2}):(\\d{2}):(\\d{2})$"), "PT$1H$2M$3S")
+                return ZERO_DATE.plus(Duration.parse(iso))
+            }
+            else -> {
+                // TODO: add support for other date/time formats
+                throw IOException("Unsupported date format: $value")
+            }
+        }
+    }
+
+    companion object {
+        // Zero date used by all MS Office products.
+        // Furthermore, MS Access seems to use the local time zone when storing dates.
+        private val ZERO_DATE = LocalDateTime.of(1899, 12, 30, 0, 0, 0, 0)
+
+        private val TIME_REGEX = Regex("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]")
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,49 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.healthmarketscience.jackcess.Database
+import com.healthmarketscience.jackcess.DatabaseBuilder
+import com.healthmarketscience.jackcess.util.ImportUtil
+import java.io.File
+import java.io.IOException
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun importFile(db: Database, file: File, tableName: String) {
+    ImportUtil.Builder(db)
+        .setDelimiter(",")
+        .setQuote('"')
+        .setTableName(tableName)
+        .setUseExistingTable(true)
+        .setHeader(true)
+        .setFilter(DefaultImportFilter(db.getTable(tableName).columns))
+        .importFile(file)
+}
+
+fun main(args: Array<String>) {
+    val inputDir = args[0]
+    val targetFile = args[1]
+
+    DatabaseBuilder.open(File(targetFile)).use { db ->
+        val files = File(inputDir).listFiles { file ->
+            file.isFile && file.canRead() && file.name.endsWith(".csv")
+        }?.sorted() ?: throw IOException("inputDir does not exists or an IO error occurred")
+
+        println("Removing data:")
+
+        for (file in files.reversed()) {
+            val tableName = file.nameWithoutExtension.removePrefix("ptv_")
+
+            println(" - ${tableName}...")
+
+            db.getTable(tableName).removeAll { true }
+        }
+
+        println("Importing data:")
+
+        for (file in files) {
+            val tableName = file.nameWithoutExtension.removePrefix("ptv_")
+
+            println(" - ${tableName}...")
+
+            importFile(db, file, tableName)
+        }
+    }
+
+    println("done")
 }
